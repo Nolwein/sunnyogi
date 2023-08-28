d6c16fb9c3b52f6e156666662f69284d10c712ee
Nolwein Delsaut <delsaut.nolwein@gmail.com>
2023-08-28 17:42:11 +0200

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  ressources :lessons, only: [:new, :create, :show, :destroy] do
-    ressources :favorites, only: [:create]
+  resources :lessons, only: [:new, :create, :show, :destroy] do
+    resources :favorites, only: [:create]
   end
 
-  ressources :favorites, only: [:destroy]
+  resources :favorites, only: [:destroy]
 
-  ressources :profiles, only: [:show]
+  resources :profiles, only: [:show]
 
   # xp user update route à déterminer (ou déjà gérée par devise_for :users)
 end
