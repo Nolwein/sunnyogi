8b68558db6ce5c6b13bddcb457a7ada2a2c55a71
Maxime Montfort consultant.blockchain@outlook.com <consultant.blockchain@outlook.com>
2023-08-30 12:37:56 +0200

    New route to profile and navbar OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "profile", to: "pages#profile"
+
 
   resources :lessons, only: [:new, :create, :show, :destroy] do
     resources :favorites, only: [:create]
   end
 
   resources :favorites, only: [:destroy]
-
-  resources :profiles, only: [:show]
 
   # xp user update route à déterminer (ou déjà gérée par devise_for :users)
 end
