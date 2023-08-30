bd8309b06a831d057e144f4573aa633af915e7ba
FlorianFaneuil <florian.faneuil@gmail.com>
2023-08-30 14:22:36 +0200

    add to favorite a lesson

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_125010) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_153254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,6 +99,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_125010) do
     t.string "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "youtube_sessions", force: :cascade do |t|
+    t.string "session_token", null: false
+    t.json "credentials"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_token"], name: "index_youtube_sessions_on_session_token"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
